1483384b3cb7f5f2821c33c2423db302bc02d411
Gepeng18 <robin_ge@foxmail.com>
2020-07-15 19:55:18 +0800

    修改报错信息

--- olexec/src/main/java/site/pyyf/service/ExecuteStringSourceService.java
+++ olexec/src/main/java/site/pyyf/service/ExecuteStringSourceService.java
@@ -88,7 +88,7 @@ public class ExecuteStringSourceService implements IExecuteStringSourceService {
         } catch (InterruptedException e) {
             runResult = "Program interrupted.";
         } catch (ExecutionException e) {
-            runResult = e.getCause().getMessage();
+            runResult = e.getMessage();
         } catch (TimeoutException e) {
             runResult = "Time Limit Exceeded.";
         } finally {
